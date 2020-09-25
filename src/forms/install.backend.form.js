b2d934c4de1328b9f7f3519409adba3553aaaff6
filvecchiato <fil.vecchiato@gmail.com>
2020-09-25 11:28:39 +0200

    feat: frontend building and running using cra
    frontend fully running using cra, git init and npm install working automatically on frontend and
    backend

---
+++ src/forms/install.backend.form.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    type: 'confirm',
+    name: 'git',
+    message: 'Initialise git in the backend?',
+    default: true
+  },
+  {
+    type: 'confirm',
+    name: 'install',
+    message: 'Install dependencies in the backend?',
+    default: true
+  }
+]
--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const fs = require('fs');
 const commonForm = require('./forms/index.form');
 const frontendForm = require('./forms/frontend.form');
 const backendForm = require('./forms/backend.form');
+const installFrontForm = require('./forms/install.frontend.form');
+const installBackForm = require('./forms/install.backend.form');
+
 const createFile = require('./utils/tools/fileWriter');
 
 
@@ -18,31 +21,31 @@ const createFile = require('./utils/tools/fileWriter');
 async function generateSubfolders(appFolderName, environment) {
   await shell.cd(`${appFolderName}_${environment}`);
   if (environment === 'backend') {
-    await shell.echo('');
-    await shell.echo('Creating Backend folder structure');
+    shell.echo('');
+    shell.echo('Creating Backend folder structure');
     await shell.exec(`mkdir controllers routers models`);
-    await shell.echo('Backend folder structure created');
-    await shell.echo('');
+    shell.echo('Backend folder structure created');
+    shell.echo('');
   } else if (environment === 'frontend') {
-    await shell.echo('');
-    await shell.echo('Creating Frontend folder structure');
-    await shell.echo('Creating frontend');
-    await shell.echo('Frontend folder structure created');
-    await shell.echo('');
+    shell.echo('');
+    shell.echo('Creating Frontend folder structure');
+    shell.echo('Creating frontend');
+    shell.echo('Frontend folder structure created');
+    shell.echo('');
   }
   // return to root folder
   await shell.cd(`..`);
-  
 }
 
 // start the app generation process
 
-async function createDirectories(appName) {
+const createDirectories = async (appName) => {
   try {
     await shell.exec(`mkdir ${appName}`);
     await shell.cd(`${appName}`)
-    await shell.exec(`mkdir ${appName}_backend ${appName}_client config`)
-    
+    await shell.exec(`mkdir ${appName}_backend config`)
+    await shell.exec(`npx create-react-app  ${appName}_client`);
+    shell.echo('')
     await generateSubfolders(appName, 'backend');
   
   } catch (e) {
@@ -61,21 +64,21 @@ async function userForms(appName) {
   };
 
   try {
-    await shell.echo(' ------ Set up the common App ------ ');
-    await shell.echo('');
+    shell.echo(' ------ Set up the common App ------ ');
+    shell.echo('');
     const common = await inquirer.prompt(commonForm);
     common.app_name.toLowerCase().split('').join('-');
     options.common = common;
 
-    await shell.echo('');
-    await shell.echo(' ------ Set up the frontend of the App ------ ');
-    await shell.echo('');
+    shell.echo('');
+    shell.echo(' ------ Set up the frontend of the App ------ ');
+    shell.echo('');
     const frontend = await inquirer.prompt(frontendForm);
     options.frontend = frontend;
 
-    await shell.echo('');
-    await shell.echo(' ------ Set up the backend of the App ------ ');
-    await shell.echo('');
+    shell.echo('');
+    shell.echo(' ------ Set up the backend of the App ------ ');
+    shell.echo('');
     const backend = await inquirer.prompt(backendForm);
     options.backend = backend;
 
@@ -87,44 +90,121 @@ async function userForms(appName) {
   }
 }
 
+//install git for selected environment
+
+const initGitFiles = async (appName, environment) => {
+  try {
+    shell.echo('');
+    shell.echo(` ------ Initialising git for ${environment} ------ `);
+    shell.echo('');
+
+    await shell.cd(`${appName}_${environment === 'frontend' ? 'client' : environment}`);
+    await shell.exec(`git init`);
+
+    shell.echo('');
+    shell.echo(` ------ ${environment} git initialised ------ `);
+    shell.echo('');
+
+    await shell.cd(`..`);
+  } catch (e) {
+    throw new Error(`could not initialise git, ${e}`);
+  }
+}
+
+// install dependencies for selected environment
+const installDependencies = async (appName, environment) => {
+  try {
+    shell.echo('');
+    shell.echo(` ------ Installing ${environment} ------ `);
+    shell.echo('');
+
+    await shell.cd(`${appName}_${environment==='frontend' ? 'client' : environment}`);
+    await shell.exec(`npm i`);
+
+    shell.echo('');
+    shell.echo(` ------ ${environment} installed ------ `);
+    shell.echo('');
+    await shell.cd(`..`);
+  } catch (e) {
+    throw new Error(`could not install dependencies, ${e}`);
+  }
+}
 // generate function of entire app folder structure
 async function generateFullStackApp(appName) {
+  const requiredDep = ['git', 'npm', 'npx'];
+
+  requiredDep.forEach(dep => {
+    if (!shell.which(dep)) {
+      shell.echo(`Sorry, this script requires ${dep}`);
+      shell.exit(1);
+    }
+  });
+  
   const types = ['common', 'backend']; //add frontend when ready
   try {
-    await shell.echo('');
-    await shell.echo(' ------ Creating Root Folder ------ ');
-    await shell.echo('');
+    shell.echo('');
+    shell.echo(' ------ Creating Root Folder ------ ');
+    shell.echo('');
 
     await createDirectories(appName);
 
-    await shell.echo('');
-    await shell.echo(' ------ Root folder created ------ ');
-    await shell.echo('');
+    shell.echo('');
+    shell.echo(' ------ Root folder created ------ ');
+    shell.echo('');
 
     const answers = await userForms(appName);
     
-
-    await shell.echo('');
-    await shell.echo(' ------ Files generation in progress ------ ');
-    await shell.echo('');
+    shell.echo('');
+    shell.echo(' ------ Files generation in progress ------ ');
+    shell.echo('');
 
     types.forEach(type => require(`./modules/${type}/common/config.json`).forEach(file => {
       createFile(answers, file, ['modules', type, 'common', 'templates'], type);
     }));
-
-    await shell.echo('');
-
-    await shell.echo(' ------ Files generation completed, happy coding! ------ ');
-    await shell.echo('');
+    
+    shell.echo('');
+    shell.echo(' ------ Files generation completed, happy coding! ------ ');
+    shell.echo('');
 
   } catch (e) {
     throw new Error(`Error creating the directory, ${e}`);
   }
+  //install frontend git and dependencies
+  try {
+    await inquirer.prompt(installFrontForm).then(async ans => {
+      if (ans.git) {
+        await initGitFiles(appName, 'frontend');
+      } else {
+        shell.echo(' ------ Skipping frontend Git Init ------ ');
+      }
 
-  // missing the creation of the dependencies in the package.json for backend and frontend
-  // check what dependencies needs installing from each config file and then run shell cd to navigate in the folder and then run exec npm install
+      if (ans.install) {
+        await installDependencies(appName, 'frontend');
+      } else {
+        shell.echo(` ------ Skipping frontend installation ------ `);
+      }
+    });
+  } catch (e) {
+    throw new Error(e)
+  }
+//install backend git and dependencies
+  try {
+    await inquirer.prompt(installBackForm).then(async ans => {
+      if (ans.git) {
+        await initGitFiles(appName, 'backend');
+      } else {
+        shell.echo(' ------ Skipping backend Git Init ------ ');
+      }
 
-  // check if git init should also run
+      if (ans.install) {
+        await installDependencies(appName, 'backend');
+      } else {
+        shell.echo(` ------ Skipping backend installation ------ `);
+      }
+    });
+  } catch (e) {
+    throw new Error(e)
+  }
 }
 
 module.exports = {
